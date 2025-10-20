db1f06444b85e21bdb7e68e13816b56208f0bee3
rayhan <mohammadrayhanx0@gmail.com>
2025-10-20 15:23:16 +0600

    somnething added

--- script.js/app.js
+++ script.js/app.js
@@ -1,9 +1,14 @@
+// what  is json and xml 
+
 // const person = {
 //   name:'hena',
 //   age: 32,
 //   friend:['rayhan','arman','akash'],
 //   status:true
 // }
+
+//number and bolean value can not  stringify
+//json is always is  a string  
 
 // const newPerson = JSON.stringify(person);
 // console.log(newPerson)
--- script.js/fetch.js
+++ script.js/fetch.js
@@ -1,8 +1,17 @@
-// fetch ('https://jsonplaceholder.typicode.com/todos/1')
-// .then(res=>res .json())
-// .then(data =>{
-//   console.log(data)
-// })
+// Json placeholder 
+
+//what is promise 
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     // it basucally parse the  promse 
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+
+// how to see json data in ineteresting way extension
+
+
 // fetch ('https://jsonplaceholder.typicode.com/todos/1')
 // .then(res=>console.log(res))
 // .then(data =>{
